8d90570ece905ef1f96ce962a2c30c35edbb823d
Macarena Ch Bykov <bykov_make@hotmail.com>
2025-10-30 16:17:13 -0300

    Implement "Add Todo"

--- SchedulerWebApp/schedulerWeb.py
+++ SchedulerWebApp/schedulerWeb.py
@@ -1,5 +1,14 @@
 import streamlit as st
+
+
 import functions
+
+def add_todo():
+    todo = st.session_state["new_todo"] + "\n"
+    print(todo)
+    todos.append(todo)
+    functions.write_todos(todos)
+
 
 todos = functions.get_todos()
 
@@ -10,4 +19,7 @@ st.write("This app is to increase productivity ...")
 for todo in todos:
     st.checkbox(todo)
 
-st.text_input(label='Enter a To Do',placeholder="Add new to-do")
+st.text_input(label='Enter a To Do',placeholder="Add new to-do",
+              on_change=add_todo, key='new_todo' )
+
+st.session_state
