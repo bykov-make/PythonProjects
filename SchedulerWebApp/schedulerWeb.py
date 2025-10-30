310ee8be6693026f572fd5f44f8895af75aeb8a8
Macarena Ch Bykov <bykov_make@hotmail.com>
2025-10-30 17:19:13 -0300

    Implement Delete Marked todo

--- SchedulerWebApp/schedulerWeb.py
+++ SchedulerWebApp/schedulerWeb.py
@@ -16,10 +16,13 @@ st.title("My Todo App")
 st.subheader("This is my todo app")
 st.write("This app is to increase productivity ...")
 
-for todo in todos:
-    st.checkbox(todo)
+for index, todo in enumerate(todos):
+    checkbox = st.checkbox(todo, key=todo)
+    if checkbox:
+        todos.pop(index)
+        functions.write_todos(todos)
+        del st.session_state[todo]
+        st.rerun()
 
 st.text_input(label='Enter a To Do',placeholder="Add new to-do",
               on_change=add_todo, key='new_todo' )
-
-st.session_state
